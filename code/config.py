b4135efe41561b6cbdb42311f021bdcf68c74af6
林思帆 <3867085448@qq.com>
2026-03-07 21:53:52 +0800

    优化路径配置

--- code/config.py
+++ code/config.py
@@ -8,6 +8,6 @@ import customtkinter as ctk
 from tkinter import messagebox
 from tkinter import filedialog
 
-
-TEMP_PATH = os.path.join('.', 'temp')   # ./temp文件夹路径
-DATA_PATH = os.path.join('.', 'data.json')  # ./data.json文件夹路径
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMP_PATH = os.path.join(BASE_DIR, 'temp')   # ./temp文件夹路径
+DATA_PATH = os.path.join(BASE_DIR, 'data.json')  # ./data.json文件夹路径
